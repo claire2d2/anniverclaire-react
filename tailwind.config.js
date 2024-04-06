4ddc59148ce1a14e6fa6377bce387addafe0cc8d
Claire Song <claireyuan.song@gmail.com>
2024-04-06 16:57:06 +0200

    added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cedarville: ['"Cedarville Cursive"', "sans-serif"],
+        yesteryear: ["Yesteryear", "sans-serif"],
+      },
+    },
   },
   plugins: [require("tw-elements/plugin.cjs")],
 };
