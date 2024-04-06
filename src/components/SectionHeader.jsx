fc2516c026aa4e013eec72ec437552af83dfa7a4
Claire Song <claireyuan.song@gmail.com>
2024-04-06 22:32:33 +0200

    added border to header

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const SectionHeader = ({ children }) => {
   return (
-    <h2 className="w-full font-limelight text-white bg-blue-800 text-3xl py-2 shadow-lg">
+    <h2 className="w-full font-limelight text-white bg-blue-800 text-3xl py-2 shadow-lg border border-white">
       {children}
     </h2>
   );
