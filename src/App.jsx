8fc5a07668cc3cdb6578dc4d205ae99fb89f396a
Claire Song <claireyuan.song@gmail.com>
2024-04-06 16:57:18 +0200

    updated main page

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,31 @@ import "./App.css";
 
 // import des components de la page
 import NavBar from "./components/NavBar";
+import BdayInfo from "./components/BdayInfo";
+import Deroule from "./components/Deroule";
 import Form from "./components/Form/Form";
 import FAQ from "./components/FAQ/FAQ";
+
+import backgroundImage from "../public/background.webp";
 
 function App() {
   return (
     <>
-      <div className="App">
-        <NavBar />
+      <div className="App w-screen">
+        <div className="w-full hidden lg:flex">
+          <NavBar />
+        </div>
 
-        <section
-          className="h-2/5 bg-local"
-          style={{ backgroundImage: `url("../public/background.webp")` }}
-        >
-          <h1 className="text-6xl font-normal leading-normal mt-0 mb-2 text-pink-600">
-            ANNIVERCLAIIIIRE !!!
+        <section className="relative h-2/5 bg-red-500">
+          <img src={backgroundImage} className="object-cover h-full" />
+          <h1 className="absolute h-10 font-cedarville top-40 text-3xl font-normal w-full leading-normal mt-0 mb-2 text-pink-600">
+            anniverglaiiiire
           </h1>
         </section>
-        <section className="h-1/5 bg-rose-100">Déroulé</section>
+        <section className="Bday-info">
+          <BdayInfo />
+        </section>
+        <section className="h-1/5 bg-rose-100"></section>
 
         <section className="h-2/5 bg-white">
           <Form />
